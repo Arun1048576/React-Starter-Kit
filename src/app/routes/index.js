b099af87b96601736af21e00050b2e5f635f8749
Arun Premkumar <arunpremkumar108@gmail.com>
2016-10-11 19:16:23 +0530

    small impovements

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import {Route} from 'react-router';
 
-if (typeof require.ensure !== 'function') require.ensure = (d, c) => c(require);
+const startComponent = (Component, callback) => {
+  const Element   = Component.default;
+  const Reducer   = Component.reducer;
+  const Action    = Component.action;
+
+  // TODO : Replace the previous Component Reducer with this Reducer
+  // TODO : Call Reducer.INITALIZE_ROUTE to get Ready for the component
+  // TODO : Action.initAction if present then resolve it and then call the following callback
+
+  callback(null, Element);
+}
 
 export default (
-  <Route  
-    path="/" 
-    getComponent={(nextState, callback)=> {
-      require.ensure([], (require)=> {
-        const Component = require('../components/Tagger/index').default;
-        callback(null, Component);
-      }, 'Tagger')
+  <Route  path="/" 
+          getComponent={(nextState, callback)=> {
+            require.ensure([], (require)=> {
+            startComponent(require('../components/Tagger/index'), callback);
+          }, 'Tagger')
     }}
   />
 )
--- webpack.config.js
+++ webpack.config.js
@@ -30,12 +30,6 @@ module.exports = {
       query         : {
         presets     : ['react-hmre']
       }
-    }, {
-      test          : /\.css$/,
-      loaders       : [
-        'style?sourceMap',
-        'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
-      ]
     }]
   }
 };
